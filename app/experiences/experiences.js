d0a579e67eca440aba4f04e38279abb990eb66ac
floKianB <kianbaban@yahoo.com>
2023-07-06 18:00:12 -0400

    experience is designed

--- app/experiences/experiences.js
+++ app/experiences/experiences.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import EachExperience from './eachExperience'
 
 function Experiences() {
     return (
         <div>
             <ol className="group/list">
-                <li className="mb-12"></li>
+                <li className="mb-12">
+                    <EachExperience 
+                        date = "2021 — Sep 2022"
+                        position = "Software Developer"
+                        at = "Awitel"
+                        loc = "Toronto, ON"
+                        description = "As a Software Developer at Awitel INC, I gained experience in developing React JS projects while assisting the design team. I contributed to innovative applications and collaborated on enhancing user interfaces."
+                    />
+                </li>
             </ol>
         </div>
     )
